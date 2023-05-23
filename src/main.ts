079224880bb963f826373d43e6c99ea0edd489d6
Combatd <combatdstar@gmail.com>
2023-05-22 19:41:06 -0700

    Import Angular features using #bootstrapModule

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,14 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+#platformBrowserDynamic will load an object with information on the current platform/browser.
+#bootstrapModule will load our app and Angular features
+AppModule will give definitions of Angular features to compile.
 */
 
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module.js";
 
-platformBrowserDynamic();
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
