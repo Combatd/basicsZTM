a32aaf99d845fb003933a793c716f9df5acb9de4
Combatd <combatdstar@gmail.com>
2023-05-26 16:33:56 -0700

    Apply decorator to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
+/**
+ import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-// import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';
 
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+ */
+
+
+/**
+ * Modules are not necessarily isolated from each other.
+ * They are a way to group files at the same time.
+ * So there will be times where we need code from other modules.
+ * We can import modules into other Angular modules.
+ */
+
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ]
+})
+export class AppModule {
+
+}
