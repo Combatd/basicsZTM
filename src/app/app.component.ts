976076d5ba10f6b22fce7f11117b0c722764e959
Combatd <combatdstar@gmail.com>
2023-05-26 18:29:00 -0700

    Create html template and style template for component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,12 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: '<p>Hello world!</p>'
+  // template: '<p>Hello world!</p>',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.css'
+  ]
+  // styles: ['']
 })
 export class AppComponent {
 
