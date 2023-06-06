f2f20e9fd0cb8f5195134e4caba8073d64e33f8a
Combatd <combatdstar@gmail.com>
2023-06-06 06:33:57 -0700

    Use component lifecycle hook ::ngOnInit

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   @Input() postImg = '';
   @Output() imgSelected = new EventEmitter<string>();
+
+  constructor() {
+    console.log('PostComponent::constructor called');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit() called');
+  }
 }
