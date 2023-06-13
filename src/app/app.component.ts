c8f0d6c5515ff63029d2e319c74a3cb9106da436
Combatd <combatdstar@gmail.com>
2023-06-13 07:54:03 -0700

    Display JSON on AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,10 @@ export class AppComponent {
   currentDate = new Date();
   cost = 2000;
   temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large'
+  };
 
   getName() {
     return this.name;
